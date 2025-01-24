0b5c5585d8169029f1c9ff7d33bcfe78b7896490
mac <mac@macs-MacBook-Air.local>
2025-01-24 20:55:54 +0700

    initial commit

---
+++ reverse.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func reverseString(str string) string {
+	runes := []rune(str)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
+
+func main() {
+	fmt.Println("reverse :", reverseString("Ramadhan Tiba"))
+}
